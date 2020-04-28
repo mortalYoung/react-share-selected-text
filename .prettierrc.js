f4c1e5ede44d885ce6f4c1e7490b0dc9e1360259
yangwei <yangwei1@outlook.com>
2020-04-28 22:32:07 +0800

    build(package):add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "printWidth": 80,
+    "tabWidth": 1,
+    "useTabs": true,
+    "jsxBracketSameLine": false,
+    "singleQuote": true,
+    "semi": true,
+    "trailingComma": "all",
+    "bracketSpacing": true,
+    "parser": "babel"
+}

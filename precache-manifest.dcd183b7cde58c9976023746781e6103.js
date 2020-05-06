5168c258095f4196e458a6e3d6fdd18d34b913bf
yangwei <yangwei1@outlook.com>
2020-05-06 16:23:11 +0800

    fix: PUBLIC_URL not found

---
+++ precache-manifest.dcd183b7cde58c9976023746781e6103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9eb2fb094fd31787f84f67d04e86a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "317af18577fbf4217de0",
+    "url": "/static/css/2.9f6a9d9a.chunk.css"
+  },
+  {
+    "revision": "ecca404270effc8f273b",
+    "url": "/static/css/main.90975527.chunk.css"
+  },
+  {
+    "revision": "317af18577fbf4217de0",
+    "url": "/static/js/2.626643f0.chunk.js"
+  },
+  {
+    "revision": "e1ebcee444532ceab7d2a4a1c7007bba",
+    "url": "/static/js/2.626643f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecca404270effc8f273b",
+    "url": "/static/js/main.0b02eb7b.chunk.js"
+  },
+  {
+    "revision": "7c1c1ffbf5b78a7fe83f",
+    "url": "/static/js/runtime-main.7f6e8320.js"
+  }
+]);

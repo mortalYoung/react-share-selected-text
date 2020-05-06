98f8fdc4b740fd5ce84307f38ff7181ed19d592b
yangwei <yangwei1@outlook.com>
2020-05-06 16:41:22 +0800

    Updates

---
+++ precache-manifest.f6ce75809d107585fc8c4eceec467a84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8748fba52686ce3e0e24e45c7d1da802",
+    "url": "/react-share-selected-text/index.html"
+  },
+  {
+    "revision": "317af18577fbf4217de0",
+    "url": "/react-share-selected-text/static/css/2.9f6a9d9a.chunk.css"
+  },
+  {
+    "revision": "b630008a1f0d7335eac5",
+    "url": "/react-share-selected-text/static/css/main.90975527.chunk.css"
+  },
+  {
+    "revision": "317af18577fbf4217de0",
+    "url": "/react-share-selected-text/static/js/2.626643f0.chunk.js"
+  },
+  {
+    "revision": "e1ebcee444532ceab7d2a4a1c7007bba",
+    "url": "/react-share-selected-text/static/js/2.626643f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b630008a1f0d7335eac5",
+    "url": "/react-share-selected-text/static/js/main.77f35174.chunk.js"
+  },
+  {
+    "revision": "1f5fc7eec165c0363860",
+    "url": "/react-share-selected-text/static/js/runtime-main.f16174c2.js"
+  }
+]);

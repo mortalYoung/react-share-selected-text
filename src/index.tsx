1d845f4bf50a361b274534a52c7af77e8e8d7b18
yangwei <yangwei1@outlook.com>
2020-05-07 16:45:26 +0800

    feat: add new parameters

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
-import React, {useEffect, useRef, useState, CSSProperties} from 'react';
+import React, { useEffect, useRef, useState, CSSProperties } from 'react';
 import classnames from 'classnames';
-import {Weibo, Wechat, Twitter, Facebook} from './icon';
-import {getSelectedText} from './utils';
+import { Weibo, Wechat, Twitter, Facebook } from './icon';
+import { getSelectedText } from './utils';
 import * as styles from './index.css';
-const innerButtons: {[key: string]: JSX.Element | null} = {
-	weibo: <Weibo />,
-	facebook: <Facebook />,
-	wechat: <Wechat />,
-	twitter: <Twitter />,
-};
+
 interface ButtonProps {
 	icon: string | JSX.Element;
 	onClick?: () => void;
@@ -18,29 +13,106 @@ interface ShareSelectedProps {
 	timeout?: number;
 	buttons?: ButtonProps[];
 	buttonsRender?: JSX.Element;
-	gridWidth?: number;
+	gridSize?: number;
 	buttonsClassName?: string;
+	onShow?: () => void;
+	onHide?: () => void;
 }
+/**
+ * 内置的 SVG Icon
+ * @param name
+ * @param size
+ */
+const getButtons = (name: string, size?: number): JSX.Element | null => {
+	const innerButtons: { [key: string]: JSX.Element | null } = {
+		weibo: <Weibo size={size} />,
+		facebook: <Facebook size={size} />,
+		wechat: <Wechat size={size} />,
+		twitter: <Twitter size={size} />,
+	};
+	return innerButtons[name];
+};
+/**
+ * 获取 buttons 的样式
+ * @param buttons
+ * @param gridSize
+ */
+const buttonsStyle = (
+	buttons: ButtonProps[] = [],
+	gridSize: number,
+): CSSProperties => {
+	if (Array.isArray(buttons) && buttons.length) {
+		return {
+			gridTemplateColumns: `repeat(${buttons.length},${gridSize}px)`,
+			gridTemplateRows: gridSize,
+		};
+	} else {
+		return {};
+	}
+};
+/**
+ * 渲染 buttons
+ * @param buttons
+ * @param gridSize
+ * @param buttonsRender
+ */
+const renderButtons = (
+	buttons: ButtonProps[] = [],
+	gridSize: number,
+	buttonsRender?: JSX.Element,
+): JSX.Element[] | JSX.Element => {
+	if (Array.isArray(buttons) && buttons.length) {
+		return buttons.map((item, index) => {
+			if (typeof item.icon === 'string') {
+				return (
+					<div
+						className={styles['shared-button']}
+						onClick={item.onClick}
+						key={item.icon}>
+						{getButtons(item.icon, gridSize - 10)}
+					</div>
+				);
+			} else {
+				return (
+					<div
+						className={styles['shared-button']}
+						onClick={item.onClick}
+						key={index}>
+						{item.icon}
+					</div>
+				);
+			}
+		});
+	} else if (buttonsRender) {
+		return <>{buttonsRender}</>;
+	}
+	return <div className={styles['empty-data']}>No Data</div>;
+};
 const SharedSelectText = ({
 	children,
 	timeout = 300,
 	buttons,
 	buttonsRender,
-	gridWidth = 30,
+	gridSize = 30,
 	buttonsClassName,
+	onShow,
+	onHide,
 }: ShareSelectedProps): JSX.Element => {
 	const slotRef = useRef<HTMLElement>(null);
 	const [visible, setVisible] = useState(false);
-	const [position, setPosition] = useState({top: 0, left: 0});
+	const [position, setPosition] = useState({ top: 0, left: 0 });
 	const updatePosition = (rect: DOMRect): void => {
 		const refRect = slotRef.current
 			? slotRef.current.getBoundingClientRect()
 			: new DOMRect(0);
 		const top = rect.top - refRect.top;
 		const left = rect.left + rect.width / 2 - refRect.left;
-		setPosition({top, left});
+		setPosition({ top, left });
 		window.setTimeout(() => {
 			setVisible(true);
+			if (onShow) {
+				onShow();
+			}
 		}, timeout);
 	};
 	const updateShared = (): void => {
@@ -54,43 +126,9 @@ const SharedSelectText = ({
 			updatePosition(oRect);
 		} else {
 			setVisible(false);
-		}
-	};
-	const renderButtons = (): JSX.Element[] | JSX.Element => {
-		if (Array.isArray(buttons) && buttons.length) {
-			return buttons.map((item, index) => {
-				if (typeof item.icon === 'string') {
-					return (
-						<div
-							className={styles['shared-button']}
-							onClick={item.onClick}
-							key={item.icon}>
-							{innerButtons[item.icon]}
-						</div>
-					);
-				} else {
-					return (
-						<div
-							className={styles['shared-button']}
-							onClick={item.onClick}
-							key={index}>
-							{item.icon}
-						</div>
-					);
-				}
-			});
-		} else if (buttonsRender) {
-			return <>{buttonsRender}</>;
-		}
-		return <div className={styles['empty-data']}>No Data</div>;
-	};
-	const buttonsStyle = (): CSSProperties => {
-		if (Array.isArray(buttons) && buttons.length) {
-			return {
-				gridTemplateColumns: `repeat(${buttons.length},${gridWidth}px)`,
-			};
-		} else {
-			return {};
+			if (onHide) {
+				onHide();
+			}
 		}
 	};
 	useEffect(() => {
@@ -103,15 +141,16 @@ const SharedSelectText = ({
 			<div
 				className={classnames(
 					styles['shared-container'],
+					buttonsClassName,
 					!visible && styles.visible,
 				)}
 				style={{
 					...position,
 				}}>
 				<div
-					className={classnames(styles['shared-buttons'], buttonsClassName)}
-					style={buttonsStyle()}>
-					{renderButtons()}
+					className={classnames(styles['shared-buttons'])}
+					style={buttonsStyle(buttons, gridSize)}>
+					{renderButtons(buttons, gridSize, buttonsRender)}
 				</div>
 			</div>
 			{children}
